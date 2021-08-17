085dc6edfc30b066bee6d0b3b5d34ce7e733f9da
Oluwafemijohn <femiogundipe01@gmail.com>
2021-08-17 19:53:28 +0100

    Task done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-
-
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -19,25 +16,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
+bool showText = false;
+String _fName = "";
+// var _phone;
+TextEditingController fNameCon = new TextEditingController();
+// final phoneCon = new TextEditingController();
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
-  var _fName;
-  var _lName;
-  var _email;
-  // var _phone;
-
-  final fNameCon = new TextEditingController();
-  final lNameCon = new TextEditingController();
-  final emailCon = new TextEditingController();
-  // final phoneCon = new TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,45 +39,24 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextField(
-              controller: fNameCon,
+                controller: fNameCon,
+                decoration: InputDecoration(
+                  hintText: "Enter your first name",
+                  contentPadding: new EdgeInsets.all(20.0),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                ),
+            ),
 
-              decoration: InputDecoration(
-                hintText: "Enter your first name",
-                contentPadding: new EdgeInsets.all( 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8)
-                )
-              ),
-            ),
-            TextField(
-              controller: lNameCon,
-              decoration: InputDecoration(
-                hintText: "Enter your last name"
-              )
-            ),
-            TextField(
-              controller: emailCon,
-              decoration: InputDecoration(
-                hintText: "Enter your email address"
-              )
-            ),
-            // TextField(
-            //   controller: phoneCon,
-            //   decoration: InputDecoration(
-            //     hintText: "Enter phone number"
-            //   )
-            // ),
-            Text("Hi, $_fName $_lName, your email is: $_email"),
-            RaisedButton(
-              onPressed: (){
+            showText ? Text("Hi $_fName, your are welcome to HNGi8") : Container(),
+            ElevatedButton(
+              onPressed: () {
                 setState(() {
                   _fName = fNameCon.text;
-                  _lName = lNameCon.text;
-                  _email = emailCon.text;
-                  // _phone = phoneCon.text as int;
+                  showText = true;
                 });
               },
-              child: Text("Submit"),
+              child: const Text("Submit"),
             ),
           ],
         ),
